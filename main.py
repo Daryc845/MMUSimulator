e56f60775be337114fb32d2e6cd9aed46146b793
Yesid Martinez <yesid.martinez04@uptc.edu.co>
2025-06-04 14:15:14 -0500

    Limpieza de codigo

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from view.gui import MMUSimulatorGUI
 import tkinter as tk
 
 def main():
+    """
+    Punto de entrada principal de la aplicación.
+    Inicializa la ventana raíz de Tkinter, crea la interfaz gráfica del simulador MMU y ejecuta el bucle principal de la GUI.
+    """
     root = tk.Tk()
     app = MMUSimulatorGUI(root)
     root.mainloop()
